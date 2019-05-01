e4778d8ca689ca7f31609f43856642bf4db1b759
hellokiii <hellokiii@naver.com>
2019-05-02 05:00:53 +0900

    스트리밍 성공

--- PythonCodes/Deum/control/urls.py
+++ PythonCodes/Deum/control/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('run/', views.run, name='run'),
 ]
