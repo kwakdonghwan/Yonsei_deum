850f7ac84759f6b2f09cbdeb2ab8f6430e0752f6
kwakdonghwan <arbiter95@naver.com>
2019-05-19 02:24:04 +0900

    데이타 읽는 함수 모두 추가완료
    이제 제어 함수만 차근차근 넣으면 된다.

--- PythonCodes/Deum/control/advenced_thermal_control.py
+++ PythonCodes/Deum/control/advenced_thermal_control.py
@@ -7,7 +7,6 @@ from datetime import datetime
 import csv
 import os
 import RPi.GPIO as GPIO
-import time #sleep함수를쓰기위해
 from socket import *
 import struct
 import threading
@@ -41,14 +40,14 @@ class Initial_condition_checker:
         edge_3 = (data[22][0] + data[22][1] + data[23][1] + data[23][0])/4
         edge_4 = (data[22][22] + data[22][23] + data[23][22] + data[23][23])/4
         edge= [ edge_1, edge_2, edge_3 , edge_4]
-        max = max(edge)
+        max1 = max(edge)
         if edge_1 == edge_2 == edge_3 == edge_4:
             return edge_1
-        elif edge_1 < max:
+        elif edge_1 < max1:
             return edge_1
-        elif edge_2 < max:
+        elif edge_2 < max1:
             return edge_2
-        elif edge_3 < max:
+        elif edge_3 < max1:
             return edge_3
         else:
             return  edge_4
@@ -110,7 +109,7 @@ class Initial_condition_checker:
         self.data_Condtion_checker(newdata)
         self.realpart_number = self.realpart_finder(newdata)
         print("(Initial_condition_checker) check is completed ")
-
+        print([self.initial_condition, self.realpart_number, self.max_tem , self,average_tem , self.min_tem , self.edge_data])
         return [self.initial_condition, self.realpart_number, self.max_tem , self,average_tem , self.min_tem , self.edge_data]
 #####################################################################################################################################
 class Microwave_contol:
@@ -177,12 +176,15 @@ class Advenced_thermal_data_control:
         self.condition_icy = 4
         self.condition_hot_and_cold = 5
         self.condition_steam = 6
+        self.condition_fire = 7
+        self.condition_fire_count = 0
 
         self.DATA_initial_data = []   # [type_flag , number_of_realpart , max_temp , average_temp , min_temp, edge_temp ]
         self.DATA_all = []    # DATA_all[0][0]    ==> ex) [[time, number_of_part, max_temp , average_temp , min_temp , edge_temp ],[time, number_of_part, max_temp , average_temp , min_temp , edge_temp]]
-        self.DATA_all_index = 0
+        self.DATA_all_index = -1
         self.DATA_operation = []  # this one will stored whan magnetron is operation.
-        self.DATA_operation_index = 0
+        self.DATA_operation_index = -1
+        self.DATA_operation_flag = True
 
         self.operation_flag = 0
         self.operation_all = 1
@@ -191,9 +193,10 @@ class Advenced_thermal_data_control:
         self.operation_turn_off = 4
         self.operation_power = 10 #0~10 will be used 0 is equal to fan_only
 
+        self.status_edge_temp = 0
+        self.status_reference_temp = [0 ,0 ]
         self.status_edge_up = [ False , 0 , 0 , 0 , 0 , 0 , 0 , False ]  #[edge_up flag , edge_up_time , number_of_part ,max_temp , average_temp ,  min_temp , edge_temp , vinyl_flag]
         self.status_operation_interval = 10
-        self.status_operation_flag = True
         self.status_min_rise = 0
         self.status_target_max = 800
         self.status_targer_next_max_or_avg = 400
@@ -210,6 +213,174 @@ class Advenced_thermal_data_control:
             cv2.destroyAllWindows()
         except:
             print("faile to close data")
+
+    def absolute_HSV_Control5(self,data4):
+        img = np.zeros((24, 32, 3), np.uint8)
+        thickness = 2
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 0.7
+        for py in range(data4.shape[0]):
+            for px in range(data4.shape[1]):
+                value_1 = 255
+                value_2 = 255
+                value_3 = 255
+                if 3000 >= data4[py][px] > 1200:  # 2000 = 200C  max = 300
+                    value_1 = 1
+                    value_2 = 255 - ((data4[py][px] - 1200) * 254 / 1000)  ## 0 = white
+                elif data4[py][px] > 1000:
+                    value_1 = 5 - ((data4[py][px] - 1000) * 4 / 200)
+                elif data4[py][px] > 700:
+                    value_1 = 15 - ((data4[py][px] - 700) * 10 / 300)
+                elif data4[py][px] > 600:
+                    value_1 = 30 - ((data4[py][px] - 600) * 15 / 100)
+                elif data4[py][px] > 500:
+                    value_1 = 45 - ((data4[py][px] - 500) * 15 / 100)
+                elif data4[py][px] > 400:
+                    value_1 = 60 - ((data4[py][px] - 400) * 15 / 100)
+                elif data4[py][px] > 300:
+                    value_1 = 75 - ((data4[py][px] - 300) * 15 / 100)
+                elif data4[py][px] > 200:
+                    value_1 = 90 - ((data4[py][px] - 200) * 15 / 100)
+                elif data4[py][px] > 150:
+                    value_1 = 105 - ((data4[py][px] - 150) * 15 / 50)
+                elif data4[py][px] > -100:
+                    value_1 = 115 - ((data4[py][px] + 100) * 10 / 250)
+                elif data4[py][px] >= -300:
+                    value_1 = 120
+                    value_3 = ((data4[py][px] + 300) * 254 / 200)  ## 0 = black
+
+                img[py][px][0] = 125 - int(value_1)  # 0~120
+                img[py][px][1] = int(value_2)
+                img[py][px][2] = int(value_3)
+
+        img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
+        img = cv2.resize(img, None, fx=15, fy=15, interpolation=cv2.INTER_CUBIC)
+
+        ################# display _data ##############################
+        display_max_temp = "max:" + str(self.DATA_all[self.DATA_all_index][2]/10)
+        display_mid_temp = "mid:" + str(self.DATA_all[self.DATA_all_index][3] / 10)
+        display_min_temp = "min:" + str(self.DATA_all[self.DATA_all_index][4] / 10)
+        display_edge_temp = "edge:" + str(self.DATA_all[self.DATA_all_index][5]/10)
+        display_time_remain_operation_time = "be over:" + str(int(self.time_remain_operation_time)) +"s"
+        display_logo = "DEUM_yonsei"
+
+        cv2.putText(img, display_max_temp, (360,30), font, fontScale, (255, 255, 255), thickness, cv2.LINE_AA)
+        cv2.putText(img, display_mid_temp, (360, 70), font, fontScale, (255, 255, 255), thickness, cv2.LINE_AA)
+        cv2.putText(img, display_min_temp, (360, 110), font, fontScale, (255, 255, 255), thickness, cv2.LINE_AA)
+        cv2.putText(img, display_edge_temp, (360, 150), font, fontScale, (255, 255, 255), thickness, cv2.LINE_AA)
+        cv2.putText(img, display_time_remain_operation_time, (360, 190), font, fontScale, (255, 255, 255), thickness, cv2.LINE_AA)
+        cv2.putText(img, display_logo, (360, 300), font, fontScale-0.2, (255, 255, 255), thickness, cv2.LINE_AA)
+
+        cv2.imshow('frame', img)
+        try:
+            cv2.moveWindow('frame' , 2, 2)
+        except:
+            print("fail_to move window")
+        cv2.waitKey(1)
+        return img
+
+    def PostProcess_edge_temp_claculator(self, data):
+        edge_1 = (data[0][0] + data[0][1] + data[1][1] + data[1][0])/4
+        edge_2 = (data[0][22] + data[0][23] + data[1][22] + data[1][23])/4
+        edge_3 = (data[22][0] + data[22][1] + data[23][1] + data[23][0])/4
+        edge_4 = (data[22][22] + data[22][23] + data[23][22] + data[23][23])/4
+        edge= [ edge_1, edge_2, edge_3 , edge_4]
+        max1 = max(edge)
+        if edge_1 == edge_2 == edge_3 == edge_4:
+            return edge_1
+        elif edge_1 < max1:
+            return edge_1
+        elif edge_2 < max1:
+            return edge_2
+        elif edge_3 < max1:
+            return edge_3
+        else:
+            return  edge_4
+    def PostProcess_Thermal_data_cut(self, data):
+        Newdata = np.zeros((24, 24), np.int16)
+        for py in range(0, 24):
+            newpx = 0
+            for px in range(4, 28):
+                if py == 1 and px == 8:
+                    Newdata[py][newpx] = data[py][px - 1]
+                else:
+                    Newdata[py][newpx] = data[py][px]
+                newpx += 1
+        return Newdata
+    def PostProcess_data_Condtion_checker(self,data):
+        max_T = np.amax(data)
+        min_T = np.amin(data)
+        reference_temp2 = self.status_edge_temp
+        if max_T > self.DATA_all[DATA_all_index][5] * 1.2 and min_T < self.status_edge_temp * 0.8:
+            self.condition_flag = self.condition_hot_and_cold
+            reference_temp1 = (3*edge_temp + max_T) / 4
+            reference_temp2 = (edge_temp+ 3*min_T ) / 4
+        elif max_T > self.status_edge_temp * 1.2:
+            self.condition_flag = self.condition_hot
+            reference_temp1 = (3*edge_temp + max_T) / 4
+        elif min_T < 70:
+            self.condition_flag = self.condition_icy
+            reference_temp1 = (2*edge_temp + min_T) / 3
+        elif min_T < self.status_edge_temp * 0.7:
+            self.condition_flag = self.condition_cool
+            reference_temp1 = (edge_temp + 4 * min_T) / 5
+        else:
+            self.condition_flag = self.condition_room
+            reference_temp1 = (3 * edge_temp + max_T) / 4
+        if self.status_edge_temp > 650:
+            self.condition_flag = self.condition_steam
+            reference_temp1 = (2*edge_temp + max_T) / 3
+        if max_T > 1400:
+            self.condition_flag  = self.condition_fire
+            if self.condition_fire_count > 2:
+                self.operation_flag = self.operation_turn_off
+            self.condition_fire_count += 1
+            print("Caution!!!! fire detected!!!!!")
+        else:
+            self.condition_fire_count = 0
+        self.status_reference_temp = [reference_temp1,reference_temp2]
+    def PostProcess_get_data(self,data):
+        Number_of_real_temp = 0
+        real_object_temp = []
+        for py in range(data.shape[0]):
+            for px in range(data.shape[1]):
+                if self.condition_flag == self.condition_room and data[py][px] > self.status_reference_temp[0]:
+                    Number_of_real_temp += 1
+                    real_object_temp.append(data[py][px])
+                elif self.condition_flag == self.condition_cool and data[py][px] < self.status_reference_temp[0]:
+                    Number_of_real_temp += 1
+                    real_object_temp.append(data[py][px])
+                elif self.condition_flag == self.condition_icy and data[py][px] < self.status_reference_temp[0]:
+                    Number_of_real_temp += 1
+                    real_object_temp.append(data[py][px])
+                elif self.condition_flag == self.condition_hot and data[py][px] > self.status_reference_temp[0]:
+                    Number_of_real_temp += 1
+                    real_object_temp.append(data[py][px])
+                elif self.condition_flag == self.condition_steam and data[py][px] > self.status_reference_temp[0]:
+                    Number_of_real_temp += 1
+                    real_object_temp.append(data[py][px])
+                elif self.condition_flag == self.condition_hot_and_cold:
+                    if data[py][px] > self.status_reference_temp[0]:
+                        Number_of_real_temp += 1
+                        real_object_temp.append(data[py][px])
+                    elif data[py][px] < self.status_reference_temp[1]:
+                        Number_of_real_temp += 1
+                        real_object_temp.append(data[py][px])
+        times = time.time() - self.time_initial_time
+        max_temp = max(real_object_temp)
+        min_temp = min(real_object_temp)
+        average_temp = s.mean(real_object_temp)
+        self.DATA_all.extend([times,Number_of_real_temp,max_temp,average_temp,min_temp,self.status_edge_temp])
+        self.DATA_all_index += 1
+        if self.DATA_operation_flag == True:
+            self.DATA_operation.extend([times,Number_of_real_temp,max_temp,average_temp,min_temp,self.status_edge_temp])
+            self.DATA_operation_index += 1
+    def PostProcess(self,data):
+        newdata = self.PostProcess_Thermal_data_cut(data)
+        self.status_edge_temp = self.PostProcess_edge_temp_claculator(newdata)
+        self.PostProcess_data_Condtion_checker(newdata)
+        self.PostProcess_get_data(newdata)
+
     def checker_vinyl_flag(self):
         if self.DATA_all[self.DATA_all_index-1][4] /self.DATA_all[self.DATA_all_index-1][2] < 0.4:
             if self.DATA_all[self.DATA_all_index][1] > self.DATA_initial_data[1] * 1.05:
@@ -223,8 +394,8 @@ class Advenced_thermal_data_control:
                     self.status_edge_up[i] = self.DATA_all[self.DATA_all_index][i-1]
                 self.status_edge_up[7] = self.checker_vinyl_flag()
     def checker_min_rise(self):
-        min_rise_pre = (self.DATA_operation[self.DATA_operation_index - 1][4] - self.DATA_operation[self.DATA_operation_index - 9][4]) / (self.DATA_operation[self.DATA_operation_index - 1][0] - self.DATA_operation[self.DATA_operation_index - 9][0])
-        min_rise = (self.DATA_operation[self.DATA_operation_index ][4] - self.DATA_operation[self.DATA_operation_index - 8][4]) / (self.DATA_operation[self.DATA_operation_index][0] - self.DATA_operation[self.DATA_operation_index - 8][0])
+        min_rise_pre = (self.DATA_operation[self.DATA_operation_index - 1][4] - self.DATA_operation[self.DATA_operation_index - 9][4]) / 8
+        min_rise = (self.DATA_operation[self.DATA_operation_index ][4] - self.DATA_operation[self.DATA_operation_index - 8][4]) / 8
         temp = (min_rise_pre + min_rise) / 2
         if temp < 1.3:
             temp = 1.3
@@ -239,4 +410,7 @@ class Advenced_thermal_data_control:
 
     def run_initialization(self,icc_data):
         self.DATA_initial_data.extend(icc_data)
+    def run_reset_time(self):
+        self.time_initial_time = time.time()
+        print("(Advenced_thermal_data_control) time_reset")
 #####################################################################################################################################
