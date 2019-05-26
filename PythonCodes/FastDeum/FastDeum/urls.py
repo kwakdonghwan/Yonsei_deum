99e5935e6faea1dc23f7f44a96f38bcb2e378221
이창민 <hellokiii@naver.com>
2019-05-27 08:00:13 +0900

    디스플레이는 완성

--- PythonCodes/FastDeum/FastDeum/urls.py
+++ PythonCodes/FastDeum/FastDeum/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-
-from django.conf.urls import url
 from django.conf.urls.static import static
+from django.conf.urls import url
+
 
 
 
@@ -27,9 +27,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('control/', include('control.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 
--- PythonCodes/FastDeum/control/main.py
+++ PythonCodes/FastDeum/control/main.py
@@ -1,4 +1,4 @@
-from status_io import *
+from .status_io import *
 import auto_contorol as auto_control
 import Temp_process_developer as Temp_process
 import advanced_thermal_control as atc
@@ -22,9 +22,7 @@ clientSock.connect((ip, port))
 print("connect success")
 
 
-
 def auto_run():
-
 
     ICC = atc.Initial_condition_checker()
     ATD = atc.Advanced_thermal_data_control()
@@ -108,16 +106,16 @@ while True:
 
     status = read_status()
     if status["on"] == 1:
-        if status["mode"] == 1:   # 자동모드
+        if status["mode"] == 1:   # automode
             auto_run()
 
-        elif status["mode"] == 0: # 수동모드
+        elif status["mode"] == 0: # manualmode
             manual_run(status["power"], status["duration"])
 
         write_status(0, 0, 0, 0, 0)
         cv2.destroyAllWindows()
 
-    # 소켓 버퍼 비우기
+
     bin_data = clientSock.recv(1536)
     # count = int(len(bin_data) / 2)
     # _ = struct.unpack('<' + ('h' * count), bin_data)
