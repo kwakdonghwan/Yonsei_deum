85f7f7afc701a1ee0551cced98d2c47239a6d3e2
hellokiii <hellokiii@naver.com>
2019-05-26 20:44:56 +0900

    need emergency stop

--- PythonCodes/FastDeum/control/status_io.py
+++ PythonCodes/FastDeum/control/status_io.py
@@ -1,6 +1,8 @@
 def read_status():
 
-    f = open("/Users/changmin/CLionProjects/Yonsei_deum/PythonCodes/FastDeum/control/status.txt", "r")  ## 절대경로로 바꾸기
+    # f = open("/Users/changmin/CLionProjects/Yonsei_deum/PythonCodes/FastDeum/control/status.txt", "r")  ## 절대경로로 바꾸기
+    f = open("/home/pi/Yonsei_deum/PythonCodes/FastDeum/control/status.txt", "r")  ## 절대경로로 바꾸기
+
     line = f.readline()
     f.close()
     mode = int(line.split(" ")[0])
@@ -13,6 +15,7 @@ def read_status():
 
 
 def write_status(mode, on, duration, power, device):
-    f = open("/Users/changmin/CLionProjects/Yonsei_deum/PythonCodes/FastDeum/control/status.txt", "w")
+    # f = open("/Users/changmin/CLionProjects/Yonsei_deum/PythonCodes/FastDeum/control/status.txt", "w")
+    f = open("/home/pi/Yonsei_deum/PythonCodes/FastDeum/control/status.txt", "w")
     f.write("{} {} {} {} {}".format(mode, on, duration, power, device))
     f.close()
